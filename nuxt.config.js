1dbd7399190f59e03d02efa14cf1e2c974d72251
Ugo <okekeugochukwu.dev@gmail.com>
2022-09-02 11:32:53 +0100

    Figured out sanity client

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,54 @@
+
+
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'App',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+      title: 'App',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Gfinity GDM Team Assignment' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'proconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'proconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap' },
+      ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Gfinity GDM Team Assignment' },
-      { name: 'format-detection', content: 'telephone=no' }
+  
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+      '@/assets/styles/main'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'proconnect', href: 'https://fonts.googleapis.com' },
-      { rel: 'proconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap' },
-    ]
-  },
+  
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
+  
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+  
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+    ],
+  
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: ['@nuxtjs/tailwindcss', "@nuxtjs/sanity"],
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/styles/main'
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxtjs/tailwindcss'],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    sanity: {
+      projectId: '21fy9g0s',
+      dataset: 'production',
+      apiVersion: '2021-03-25',
+      useCdn: true,
+    },
+  
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    }
 }
+
--- pages/index.vue
+++ pages/index.vue
@@ -18,9 +18,30 @@
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+  const sanityClient = require('@sanity/client')
+  const client = sanityClient({
+    projectId: '21fy9g0s',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: true,
+  })
+  const query = '*[_type == "fifaCard"]'
+
+  client.fetch(query).then ((cards) => {
+    console.log(cards)
+  })
+  
+
+  export default {
+    name: 'IndexPage',
+
+    data() {
+      return {
+        players: []
+      }
+    },
+  
+  }
 </script>
 
 
